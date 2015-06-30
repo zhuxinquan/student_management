36f4781ff05c7aaf87534e462b5694a08be993e7
zhuxinquan <1174272148@qq.com>
2015-06-30 17:44:08 +0800

    add my_getch

--- my_getch.h
+++ my_getch.h
@@ -1,25 +1,20 @@
 #include<stdio.h>
-#include<termios.h>
 #include<unistd.h>
+#include<termios.h>
 #include<assert.h>
 #include<string.h>
-
-
-int mygetch()
+int mygetch(void)
 {
- int c=0;
- struct termios org_opts, new_opts;
-    int res=0;
-    //-----  store old settings -----------
-     res=tcgetattr(STDIN_FILENO, &org_opts);
-     assert(res==0);
-   //---- set new terminal parms --------
-  memcpy(&new_opts, &org_opts, sizeof(new_opts));
-  new_opts.c_lflag &= ~(ICANON | ECHO | ECHOE | ECHOK | ECHONL | ECHOPRT | ECHOKE | ICRNL);
-  tcsetattr(STDIN_FILENO, TCSANOW, &new_opts);
-  c=getchar();
-   //------  restore old settings ---------
-  res=tcsetattr(STDIN_FILENO, TCSANOW, &org_opts);
-  assert(res==0);
-  return c;
+    int c=0;
+    int res = -1;
+    struct termios org_opts,new_opts;
+    res = tcgetattr(STDIN_FILENO, &org_opts);
+    assert(res == 0);
+    memcpy(&new_opts, &org_opts, sizeof(org_opts));
+    new_opts.c_lflag &=~(ICANON | ECHO |ECHOE|ECHOK|ECHONL|ECHOPRT|ECHOKE);
+    tcsetattr(STDIN_FILENO,TCSANOW,&new_opts);
+    c=getchar();
+    res=tcsetattr(STDIN_FILENO,TCSANOW,&org_opts);
+    assert(res == 0);
+    return c;
 }
